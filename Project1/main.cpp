6c604ad29594a078eb31c128e95fee9406f4330a
AHzhangwen <42054480+AHzhangwen@users.noreply.github.com>
2023-05-12 15:03:01 +0800

    包展开解析

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -49,4 +49,53 @@ int main()
     return 0;
 }
 
+#if 0
+#include <iostream>
+#include <vector>
+#include <any>
+#include <typeinfo>
+
+// 定义一个函数模板，使用包展开和折叠将所有参数保存到一个std::vector<std::any>对象中
+template<typename... Args>
+void print_arguments(Args&&... args) {
+    std::vector<std::any> argument_vector{ std::any(args)... };
+
+    std::cout << "Arguments:\n";
+    for (auto&& arg : argument_vector) {
+        std::cout << "- Type: ";
+        if (arg.has_value()) {
+            const std::type_info& type = arg.type();
+            if (type == typeid(int)) {
+                std::cout << "int, "<<std::any_cast<int>(arg);
+            } else if (type == typeid(double)) {
+                std::cout << "double, "<<std::any_cast<double>(arg);
+            } else if (type == typeid(std::string)) {
+                std::cout << "std::string, "<<std::any_cast<std::string>(arg);
+            } else if (type == typeid(char *))
+            {
+                 std::cout << "char * ,"<<std::any_cast<char *>(arg);
+            } else if (type == typeid(const char *))
+            {
+                 std::cout << "const char *  ,"<<std::any_cast<const char *>(arg);
+            }
+            //std::cout << "Value: " << std::any_cast<double>(arg) << std::endl;
+        } else {
+            std::cout << "undefined" << std::endl;
+        }
+    }
+}
+
+
+int main() {
+    int i = 42;
+    double d = 3.1415926;
+    std::string s = "Hello, world!";
+
+    // 调用print_arguments函数，传入不同类型和个数的参数
+    print_arguments(i, d, s, "test", 1, 2.0);
+
+    return 0;
+}
+
+#endif
 
